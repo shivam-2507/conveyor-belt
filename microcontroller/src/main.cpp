c366e10d1764574bbd4405d2f716199a81fbe874
Panth <pd27pate@uwaterloo.ca>
2025-11-04 22:15:29 -0500

    watchdog timer implmentation

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 #include <pthread.h>
 
-// Watchdog timer timeout in seconds
-#define WDT_TIMEOUT 5
-#define DISTANCE_THRESHOLD 10.0
-
 const int trigPin = 5;
 const int echoPin = 12;
 
@@ -16,99 +12,81 @@ long duration;
 float distanceCm;
 float distanceInch;
 bool watchdog_kicked = false;
+
 pthread_mutex_t mutex;
 pthread_t tid;
-unsigned long timer1 = 0;
-unsigned long timer2 = 0;
 
+unsigned long timer1_start = 0;
+unsigned long timer2_start = 0;
 
 void setup()
 {
   Serial.begin(115200);     // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
-
-  // MAYBE GET RID OF THIS
-  Serial.println("Configuring WDT...");
-  esp_task_wdt_init(WDT_TIMEOUT, true); // Enable panic so ESP32 restarts
-  esp_task_wdt_add(NULL);               // Add current thread to WDT watch
-  Serial.println("WDT configured successfully!");
+  
+  pthread_mutex_init(&mutex, NULL);
+  
+  pthread_create(&tid, NULL, timer2, NULL);
 }
 
-void create_threads(){
-  pthread_create( &tid, NULL, timer2, NULL);
-}
-
-// timer2
-void *timer2(void * args){
-
-  while(1){
-    if(watchdog_kicked = true)
+// timer2 for the watchdog thread
+void *timer2(void *args)
+{
+  while(1)
+  {
+    pthread_mutex_lock(&mutex);
+    bool kicked = watchdog_kicked;
+    pthread_mutex_unlock(&mutex);
+    
+    if(kicked == true)
+    {
+      unsigned long current_time = millis();
+      if(current_time - timer2_start > 3000) // 3 seconds
+      {
+        unsigned long elapsed = timer1_start - timer2_start;
+        Serial.print("Object detection time: ");
+        Serial.println(elapsed);
+      }
+    }
   }
-  unsigned long current_time = millis();
-
-
-
+  
+  return NULL;
 }
 
-float readDistance(){
-
+float readDistance()
+{
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
+  
   duration = pulseIn(echoPin, HIGH);
-
   distanceCm = duration * SOUND_SPEED / 2;
-
   Serial.print("Distance (cm): ");
   Serial.println(distanceCm);
-
+  
   return distanceCm;
 }
 
-
-// watchdog helper function
-void watchdog_kicking(void){
-  watchdog_kicked = true;
-  if(distanceCm > 10 && time > 3){
-
-  }
-  return;
-}
-
-//timer 1
-int timer1(float distanceCm)
-{
-  timer1 = millis();
-
-  if(distance > 5){
-    pthread_mutex_lock(&mutex);
-    watchdog_kicked = true;
-    pthread_mutex_unlock(&mutex);
-  } 
-  
-  unsigned long current_time = millis();
-  timer1 = current_time - timer1;
-  return timer1;
-
-
-}
-
-
-//ending the project showcase
-void end_loop(){
-  pthread_join( &tid, NULL);
-
-  pthread_mutex_destroy(&mutex);
-  return;
-}
-
-
-
 void loop()
 {
-
+  distanceCm = readDistance();
+  
+  // we start timer1 when object detected
+  if(distanceCm < 3 && timer1_start == 0)
+  {
+    timer1_start = millis();
+    Serial.println("Timer1 started");
+  }
+  
+  // Kick watchdog if distance > 5 during detection --> to let the thread know check timer2
+  if(distanceCm > 5 && timer1_start != 0)
+  {
+    pthread_mutex_lock(&mutex);
+    watchdog_kicked = true;
+    timer2_start = millis();
+    pthread_mutex_unlock(&mutex);
+  }
 }
